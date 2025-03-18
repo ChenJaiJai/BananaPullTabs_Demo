19e2cceaad7bc40f8a1622d2093a0b2a4ac1f2ac
JaiJai <wm.rd01@gmail.com>
2025-03-18 15:53:23 +0800

    autoPlay的按鈕拔除

--- index.675c0.js
+++ index.675c0.js
@@ -1,4 +1,4 @@
-System.register(["./application.de722.js"], function (_export, _context) {
+System.register(["./application.1afd5.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
