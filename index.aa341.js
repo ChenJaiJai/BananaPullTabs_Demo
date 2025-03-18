6630587c7627fc6404e191482d08245648e296b4
JaiJai <wm.rd01@gmail.com>
2025-03-18 15:27:38 +0800

    小修改，再次上傳

--- index.aa341.js
+++ index.aa341.js
@@ -1,4 +1,4 @@
-System.register(["./application.cc7d3.js"], function (_export, _context) {
+System.register(["./application.de722.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
